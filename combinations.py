6371b07498567683298f1963ae8effc38d615ed4
Pavlo Muts <pavlo.muts@gmail.com>
2021-08-04 22:12:57 +0200

    sort total trip price

--- combinations.py
+++ combinations.py
@@ -26,58 +26,54 @@ def get_data(path_to_file):
 
     reader = csv.DictReader(data_file)
 
-    # set of flight numbers used to remove duplicates in given timetable
-    flight_numbers = set()
-
-    # construct time table without duplicates
     time_table = []
+
     for flight in reader:
-        if flight['flight_no'] not in flight_numbers:
+        flight['departure'] = datetime.datetime.fromisoformat(
+            flight['departure'])
+        flight['arrival'] = datetime.datetime.fromisoformat(
+            flight['arrival'])
 
-            flight_numbers.add(flight['flight_no'])
+        # convert some fields from string into numbers
+        flight['base_price'] = float(flight['base_price'])
+        flight['bag_price'] = float(flight['bag_price'])
+        flight['bags_allowed'] = int(flight['bags_allowed'])
 
-            flight['departure'] = datetime.datetime.fromisoformat(
-                flight['departure'])
-            flight['arrival'] = datetime.datetime.fromisoformat(
-                flight['arrival'])
-
-            # convert some fields from string into numbers
-            flight['base_price'] = float(flight['base_price'])
-            flight['bag_price'] = float(flight['bag_price'])
-            flight['bags_allowed'] = int(flight['bags_allowed'])
-
-            time_table.append(flight)
+        time_table.append(flight)
 
     # todo make validation of data
 
     return time_table
 
 
-def add_trip_data(routes, bags_count):
-    extended_routes = []
+def add_trip_data(routes, origin, destination, bags_count):
+    ext_data_routes = [] # list of all routes with additinal info
 
     for flights in routes:
         travel_time = str(flights[-1]['arrival'] - flights[0]['departure'])
-
-        # datetime format back to string
+        
+        # format datetime back to string such that json can serialize it
         for flight in flights:
             flight['departure'] = flight['departure'].isoformat()
             flight['arrival'] = flight['arrival'].isoformat()
 
-        extended_route = {}
-        extended_route['flights'] = flights
-        extended_route['bags_allowed'] = min(item['bags_allowed'] for item in flights)
-        extended_route['bags_count'] = bags_count
-        extended_route['destination'] = flights[-1]['destination']
-        extended_route['origin'] = flights[0]['origin']
-        extended_route['total_price'] = sum(
+        single_route = {}
+        single_route['flights'] = flights
+        single_route['bags_allowed'] = min(item['bags_allowed'] for item in flights)
+        single_route['bags_count'] = bags_count
+        single_route['destination'] = destination
+        single_route['origin'] = origin
+        single_route['total_price'] = sum(
             item['base_price'] + bags_count * item['bag_price'] for item in flights)
 
-        extended_route['travel_time'] = travel_time
+        single_route['travel_time'] = travel_time
 
-        extended_routes.append(extended_route)
+        ext_data_routes.append(single_route)
+    
+    # sort routes by their total price
+    ext_data_routes.sort(key=lambda x: x['total_price'])
 
-    return extended_routes
+    return ext_data_routes
 
 
 def construct_routes(origin, destination, bags_count, time_table):
@@ -86,7 +82,6 @@ def construct_routes(origin, destination, bags_count, time_table):
     intermediate_routes = []
 
     for flight in time_table:
-        print(flight)
         if flight['origin'] == origin:
             if flight['destination'] == destination:
                 final_routes.append([flight])
@@ -97,6 +92,9 @@ def construct_routes(origin, destination, bags_count, time_table):
         current_route = intermediate_routes.pop()
         airports_visited = set(item['origin'] for item in current_route)
 
-    result = add_trip_data(final_routes, bags_count)
+    # populate the results with additinal info
+    result = add_trip_data(final_routes, origin, destination, bags_count)
+
+    # convert to JSON
     res = json.dumps(result, indent=4)
     print(res)
